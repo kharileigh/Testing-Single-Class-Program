8c3bf348c16012e7362d3bd46af2ded9ab77b313
kharileigh <kharileighmiller@gmail.com>
2024-08-14 11:25:40 +0100

    Testing check(text) function -- all 7 tests passed

--- lib/grammar_stats.py
+++ lib/grammar_stats.py
@@ -2,4 +2,11 @@ class GrammarStats():
 
     def check(self, text):
 
-        raise Exception("Text must have content, please enter")
+        punctuation = ".!?"
+
+        if text == "":
+            raise Exception("Text must have content, please enter")
+        
+        if text[0].isupper() and text[-1] in punctuation:
+            return True
+        return False
--- tests/test_grammar_stats.py
+++ tests/test_grammar_stats.py
@@ -16,25 +16,63 @@ def test_check_empty_text():
 
 
 '''
-ARG : text beginning capital letter
+ARG : text beginning capital letter  
 RETURN : True
 '''
+def test_check_begins_with_capital_letter():
+    grammar = GrammarStats()
+    grammar.check("Love is a beautiful thing")
+    assert True
+
+
 
 '''
 ARG : text beginning with lowercase letter
+RETURN : False
+'''
+def test_check_begins_with_lowercase_letter():
+    grammar = GrammarStats()
+    result = grammar.check("the ocean is blue")
+
+    # ---- USE VARIABLE TO FIX ASSERTION ERROR WHEN USING BOOLEAN
+    assert result == False
+
+'''
+ARG : text beginning capital letter + ends with correct puncation  
 RETURN : True
 '''
+def test_check_begins_with_capital_letter_ends_good_punctuation():
+    grammar = GrammarStats()
+    grammar.check("Love is a beautiful thing!")
+    assert True
+
+'''
+ARG : text beginning capital letter + ends with no punctuation  
+RETURN : True
+'''
+def test_check_begins_with_capital_letter_ends_no_punctuation():
+    grammar = GrammarStats()
+    result = grammar.check("Love is a beautiful thing")
+    assert result == False
 
 '''
 ARG : text beginning with a number
-RETURN : True
+RETURN : False
 '''
+def test_check_with_number():
+    grammar = GrammarStats()
+    result = grammar.check("3627904")
+    assert result == False
+
 
 '''
 ARG : text beginning with a special character
-RETURN : True
+RETURN : False
 '''
-
+def test_check_beginning_with_special_character():
+    grammar = GrammarStats()
+    result = grammar.check("!bhf")
+    assert result == False
 
 
 
